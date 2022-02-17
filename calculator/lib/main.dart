86077f00697dee60aaff36e3a2653d29b352475e
xDELINx <delind257@gmail.com>
2022-02-17 21:48:19 +0530

    button design has created

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,115 +1,124 @@
+import 'package:calculator/widgets/CalculatorButton.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Calculator());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Calculator extends StatelessWidget {
+  const Calculator({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CalculatorScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CalculatorScreenState createState() => _CalculatorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  int firstNum = 0;
+  int secondNum = 0;
+  String history = "";
+  String textToDisplay = "";
+  String res = "";
+  String operation = "";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void btnOnClick(String number) {
+    // print(number);
+    if (number == "C") {
+      textToDisplay = "";
+      firstNum = 0;
+      secondNum = 0;
+      res = "";
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.blueGrey,
+      body: Container(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              child: Padding(
+                padding: EdgeInsets.only(right: 12.0),
+                child: Text(
+                  "123",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 197, 181, 181),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  "123",
+                  style: TextStyle(
+                    fontSize: 48,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              alignment: Alignment(1.0, 1.0),
+            ),
+            Row(
+              children: [
+                CalculatorButton("AC", 0xff718571, btnOnClick),
+                CalculatorButton("C", 0xff718571, btnOnClick),
+                CalculatorButton("<", 0xff718571, btnOnClick),
+                CalculatorButton("/", 0xff718571, btnOnClick),
+              ],
+            ),
+            Row(
+              children: [
+                CalculatorButton("9", 0xff718571, btnOnClick),
+                CalculatorButton("8", 0xff718571, btnOnClick),
+                CalculatorButton("x", 0xff718571, btnOnClick),
+                CalculatorButton("+", 0xff718571, btnOnClick),
+              ],
+            ),
+            Row(
+              children: [
+                CalculatorButton("6", 0xff718571, btnOnClick),
+                CalculatorButton("5", 0xff718571, btnOnClick),
+                CalculatorButton("4", 0xff718571, btnOnClick),
+                CalculatorButton("-", 0xff718571, btnOnClick),
+              ],
+            ),
+            Row(
+              children: [
+                CalculatorButton("3", 0xff718571, btnOnClick),
+                CalculatorButton("2", 0xff718571, btnOnClick),
+                CalculatorButton("1", 0xff718571, btnOnClick),
+                CalculatorButton("+", 0xff718571, btnOnClick),
+              ],
+            ),
+            Row(
+              children: [
+                CalculatorButton("+/-", 0xff718571, btnOnClick),
+                CalculatorButton("0", 0xff718571, btnOnClick),
+                CalculatorButton("00", 0xff718571, btnOnClick),
+                CalculatorButton("=", 0xff718571, btnOnClick),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
